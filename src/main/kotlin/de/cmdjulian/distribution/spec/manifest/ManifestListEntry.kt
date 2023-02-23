03f44013b40ec122a6dc78ac3c3ff1fe99ef546f
Julian Goede <julian.goede@pm.me>
2023-02-23 23:25:46 +0100

    tests
    todo:
    add password to proxy config

--- src/main/kotlin/de/cmdjulian/distribution/spec/manifest/ManifestListEntry.kt
+++ src/main/kotlin/de/cmdjulian/distribution/spec/manifest/ManifestListEntry.kt
@@ -3,5 +3,5 @@ package de.cmdjulian.distribution.spec.manifest
 import de.cmdjulian.distribution.model.Digest
 
 data class ManifestListEntry(val mediaType: String, val digest: Digest, val size: UInt, val platform: Platform?) {
-    data class Platform(val os: String, val architecture: String, val features: List<String>?)
+    data class Platform(val os: String, val architecture: String, val features: List<String>? = null)
 }
